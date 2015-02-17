d9f84387c637b3ac639415555de81cf733f67db4
Nícholas André <nicholas@iotecnologia.com.br>
2015-02-17 18:05:28 -0300

    Módulo 7

---
+++ mod_7/Biblioteca.php
@@ -0,0 +1,104 @@
+<?php
+class BibliotecaIterator implements  Iterator {
+    private $pos = 0;
+    private $livros;
+
+    public function __construct( $livros ) {
+        $this->livros = $livros;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     */
+    public function current()
+    {
+        echo "Método current() <br />";
+        return $this->livros[$this->pos];
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     */
+    public function next()
+    {
+        echo "Método next <br />";
+        ++$this->pos;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key()
+    {
+        echo "Metodo key <br />";
+        return $this->pos;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     */
+    public function valid()
+    {
+        echo "Método valid <br />";
+        return isset($this->livros[$this->pos]);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     */
+    public function rewind()
+    {
+        echo "Método rewind <br />";
+        $this->pos = 0;
+    }
+}
+
+class Biblioteca implements IteratorAggregate, Countable {
+
+    public function fetchData() {
+        return ["PHP Programming", "Python Programming", "C++ Primer Plus", "Compiladores"];
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Retrieve an external iterator
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     * @return Traversable An instance of an object implementing <b>Iterator</b> or
+     * <b>Traversable</b>
+     */
+    public function getIterator()
+    {
+        return new BibliotecaIterator($this->fetchData());
+    }
+
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     */
+    public function count()
+    {
+        return count($this->fetchData());
+    }
+}
